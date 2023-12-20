72297a078d8c953430a898ce7b41960c3fde113b
Lu Azevedo <luciana@dags.com.br>
2023-12-20 09:09:42 -0300

    add navbar

--- template.rb
+++ template.rb
@@ -22,6 +22,54 @@ def add_home
   route "root to: 'home#index'"
 end
 
+def add_navbar
+  navbar = 'app/views/layouts/_navbar.html.erb'
+  FileUtils.touch(navbar)
+  inject_into_file 'app/views/layouts/application.html.erb', before: '<%= yield %>' do
+    "\n<%= render 'layouts/navbar' %>\n"
+  end
+  append_to_file navbar do
+    '<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <%= link_to "Rails.application.class.parent_name", root_path, class:"navbar-brand"%>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <%= link_to "Home", root_path, class:"nav-link" %>
+            </li>
+            <li class="nav-item">
+                <%= link_to "Blog", articles_path, class:"nav-link" %>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <% if current_user %>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <%= current_user.username %>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <%= link_to "Account Settings", edit_user_registration_path, class:"dropdown-item" %>
+                    <div class="dropdown-divider"></div>
+                    <%= link_to "Logout", destroy_user_session_path, method: :delete, class:"dropdown-item" %>
+                </div>
+            </li>
+            <% else %>
+            <li class="nav-item">
+                <%= link_to "Create An Account", new_user_registration_path, class:"nav-link" %>
+            </li>
+            <li class="nav-item">
+                <%= link_to "Login", new_user_session_path, class:"nav-link" %>
+            </li>
+            <% end %>
+        </ul>
+    </div>
+    </nav>'
+  end
+end
+
 def add_rails_admin
   add_gem 'rails_admin', '~> 3.0'
   run "bundle install"
@@ -46,6 +94,7 @@ add_home
 add_login
 add_tests
 add_rails_admin
+add_navbar
 
 generate(:scaffold, "article", "title:string")
 

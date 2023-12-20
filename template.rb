a8c0d61e0cf4131d204b6667473df232ce15ff9a
Lu Azevedo <luciana@dags.com.br>
2023-12-20 07:47:19 -0300

    cancan added

--- template.rb
+++ template.rb
@@ -1,13 +1,17 @@
-# rails new blog -m ./template.rb
+# rails new blog -j esbuild --css bootstrap -m ./template.rb
+# rails new blog -d postgresql -T --edge --css tailwind -m ./template.rb
 
 
 def add_gem(name, *options)
   gem(name, *options) unless gem_exists?(name)
 end
 
-def add_devise
+def add_login
   add_gem 'devise'
+  add_gem 'cancancan'
+  run "bundle install"
   generate 'devise:install'
+  generate 'cancan:ability'
   environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000}",
               env: 'development'
   generate :devise, 'User', 'username:string:uniq', 'admin:boolean'
@@ -33,7 +37,7 @@ def gem_exists?(name)
 end
 
 add_home
-add_devise
+add_login
 add_tests
 
 generate(:scaffold, "article", "title:string")
